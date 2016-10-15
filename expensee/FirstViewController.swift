f400db5dd1db02525613e71106e2712a8a456d51
Kapil Garg <mail.kapilgarg@gmail.com>
2016-10-15 17:16:25 +0530

    Initializing Views

--- expensee/FirstViewController.swift
+++ expensee/FirstViewController.swift
@@ -8,18 +8,44 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate {
+// MARK: Properties
+    
+    @IBOutlet weak var expenseDesc: UITextField!
+    @IBOutlet weak var expenseAmt: UITextField!
+    @IBOutlet weak var expensesTable: UITableView!
+    weak var expenses = [];
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        expenseDesc.delegate = self;
+        expenseAmt.delegate = self;
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+// MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder();
+        return true;
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField == expenseDesc{
+            expenseDesc.text = textField.text;
+        }else if textField == expenseAmt{
+            expenseAmt.text = textField.text;
+        }
+    }
+    
+// MARK: Actions
 
-
+    @IBAction func expenseSubmit(_ sender: UIButton) {
+//        var rowCount: Int = (expenses?.count)!;
+//        expensesTable.insertRows(at: rowCount, with: nil);
+    }
 }
 
